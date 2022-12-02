785f100710a8477bb2e36397a5c6abb5862107cb
Jake Thompson <jake@zenlist.com>
2022-12-01 22:30:42 -0500

    Cleanup

--- lib/day01.ex
+++ lib/day01.ex
@@ -5,15 +5,13 @@ defmodule Aoc2022elixir.Day01 do
     caloric_results =
       contents
       |> String.split("\n\n")
-      |> Enum.map(fn calories ->
-        String.split(calories, "\n", trim: true) |> Enum.map(&String.to_integer/1) |> Enum.sum()
+      |> Enum.map(fn
+        calories ->
+          String.split(calories, "\n", trim: true) |> Enum.map(&String.to_integer/1) |> Enum.sum()
       end)
       |> Enum.sort(:desc)
 
-    [solution | _] = caloric_results
-    Logger.info("solved", solution: solution, part: :one)
-
-    solution = caloric_results |> Enum.take(3) |> Enum.sum()
-    Logger.info("solved", solution: solution, part: :two)
+    Logger.info("solved", solution: caloric_results |> Enum.at(0), part: :one)
+    Logger.info("solved", solution: caloric_results |> Enum.take(3) |> Enum.sum(), part: :two)
   end
 end
