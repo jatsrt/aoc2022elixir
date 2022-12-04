b727dd2bf6cb382513e30c1eb6231dfc4049f90c
Jake Thompson <jake@zenlist.com>
2022-12-04 13:16:27 -0500

    I like this better

--- lib/day04.ex
+++ lib/day04.ex
@@ -2,29 +2,27 @@ defmodule Aoc2022elixir.Day04 do
   require Logger
 
   def run(contents) do
-    areas =
+    overlaps =
       String.split(contents, "\n")
       |> Enum.map(fn pair ->
         String.split(pair, ",")
         |> Enum.map(fn area ->
-          String.split(area, "-") |> Enum.map(&String.to_integer/1)
+          [s, e] = String.split(area, "-") |> Enum.map(&String.to_integer/1)
+          MapSet.new(s..e)
         end)
       end)
+      |> Enum.map(fn [x, y] ->
+        {MapSet.size(MapSet.intersection(x, y)), min(MapSet.size(x), MapSet.size(y))}
+      end)
 
-    solution = Enum.reduce(areas, 0, &contains/2)
+    solution =
+      Enum.reduce(overlaps, 0, fn {count, min}, acc -> if count >= min, do: acc + 1, else: acc end)
+
     Logger.info("solved", solution: solution, part: :one)
 
-    solution = Enum.reduce(areas, 0, &overlaps/2)
+    solution =
+      Enum.reduce(overlaps, 0, fn {count, _}, acc -> if count > 0, do: acc + 1, else: acc end)
+
     Logger.info("solved", solution: solution, part: :two)
   end
-
-  defp contains([[s1, e1], [s2, e2]], acc) when s2 <= s1 and e2 >= e1, do: acc + 1
-  defp contains([[s1, e1], [s2, e2]], acc) when s1 <= s2 and e1 >= e2, do: acc + 1
-  defp contains(_, acc), do: acc
-
-  defp overlaps([[s1, e1], [s2, _]], acc) when s2 >= s1 and s2 <= e1, do: acc + 1
-  defp overlaps([[s1, e1], [_, e2]], acc) when e2 >= s1 and e2 <= e1, do: acc + 1
-  defp overlaps([[s1, _], [s2, e2]], acc) when s1 >= s2 and s1 <= e2, do: acc + 1
-  defp overlaps([[_, e1], [s2, e2]], acc) when e1 >= s2 and e1 <= e2, do: acc + 1
-  defp overlaps(_, acc), do: acc
 end
