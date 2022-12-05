9d311849e594373639e408668acb3c1c79705dba
Jake Thompson <jake@zenlist.com>
2022-12-05 09:03:49 -0500

    Cleanup

--- lib/day05.ex
+++ lib/day05.ex
@@ -3,14 +3,17 @@ defmodule Aoc2022elixir.Day05 do
 
   def run(contents) do
     [raw_stacks, raw_moves] =
-      String.split(contents, "\n\n", trim: true) |> Enum.map(&String.split(&1, "\n"))
+      contents |> String.split("\n\n", trim: true) |> Enum.map(&String.split(&1, "\n"))
 
-    stack_rows =
-      Enum.map(raw_stacks, fn row ->
-        String.codepoints(row)
+    stacks =
+      raw_stacks
+      |> Enum.map(fn row ->
+        row
+        |> String.codepoints()
         |> Enum.chunk_every(4)
         |> Enum.map(&Enum.join/1)
         |> Enum.map(&String.trim/1)
+        |> Enum.map(&String.replace(&1, ["[", "]"], ""))
       end)
       |> List.zip()
       |> Enum.map(&Tuple.to_list/1)
@@ -19,7 +22,8 @@ defmodule Aoc2022elixir.Day05 do
       |> Enum.reduce(%{}, fn {v, k}, acc -> Map.put(acc, k, v) end)
 
     moves =
-      Enum.map(raw_moves, &String.split(&1, " "))
+      raw_moves
+      |> Enum.map(&String.split(&1, " "))
       |> Enum.map(fn move ->
         %{
           c: Enum.at(move, 1) |> String.to_integer(),
@@ -28,19 +32,21 @@ defmodule Aoc2022elixir.Day05 do
         }
       end)
 
-    stacks = moves |> Enum.reduce(stack_rows, &ms9000/2)
-    print_solution(stacks, :one)
-    stacks = moves |> Enum.reduce(stack_rows, &ms9001/2)
-    print_solution(stacks, :two)
-  end
-
-  defp print_solution(stacks, part) do
     solution =
-      Enum.map(stacks, fn {_, [v | _]} -> v end)
+      moves
+      |> Enum.reduce(stacks, &ms9000/2)
+      |> Enum.map(fn {_, [v | _]} -> v end)
       |> Enum.join("")
-      |> String.replace(["[", "]"], "")
 
-    Logger.info("solved", solution: solution, part: part)
+    Logger.info("solved", solution: solution, part: :one)
+
+    solution =
+      moves
+      |> Enum.reduce(stacks, &ms9001/2)
+      |> Enum.map(fn {_, [v | _]} -> v end)
+      |> Enum.join("")
+
+    Logger.info("solved", solution: solution, part: :two)
   end
 
   defp ms9000(%{c: 0}, stacks), do: stacks
@@ -49,14 +55,16 @@ defmodule Aoc2022elixir.Day05 do
   defp m9000(stacks, f, t) do
     [item | from_rest] = Map.get(stacks, f)
 
-    Map.put(stacks, t, [item | Map.get(stacks, t)])
+    stacks
+    |> Map.put(t, [item | Map.get(stacks, t)])
     |> Map.put(f, from_rest)
   end
 
   defp ms9001(%{c: c, f: f, t: t}, stacks) do
     {items, from_rest} = stacks |> Map.get(f) |> Enum.split(c)
 
-    Map.put(stacks, t, Enum.concat(items, Map.get(stacks, t)))
+    stacks
+    |> Map.put(t, Enum.concat(items, Map.get(stacks, t)))
     |> Map.put(f, from_rest)
   end
 end
