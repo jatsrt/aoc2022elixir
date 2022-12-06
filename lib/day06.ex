9dd5bf47f07687282c3f02aa30bcbf9ed5bac8c5
Jake Thompson <jake@zenlist.com>
2022-12-06 12:56:07 -0500

    Better

--- lib/day06.ex
+++ lib/day06.ex
@@ -4,16 +4,16 @@ defmodule Aoc2022elixir.Day06 do
   def run(contents) do
     sequence = contents |> String.split("\n", trim: true) |> Enum.at(0) |> String.codepoints()
     {seq, index} = sequence |> find_code(4)
-    Logger.info("solved: #{seq |> Enum.join()}", solution: index + 5, part: :one)
+    Logger.info("solved: #{seq |> Enum.join()}", solution: index + 4, part: :one)
 
     {seq, index} = sequence |> find_code(14)
-    Logger.info("solved: #{seq |> Enum.join()}", solution: index + 15, part: :two)
+    Logger.info("solved: #{seq |> Enum.join()}", solution: index + 14, part: :two)
   end
 
   defp find_code(seq, length) do
     Stream.chunk_every(seq, length, 1, :discard)
     |> Stream.with_index()
-    |> Stream.take_while(fn {chunk, _} -> length(chunk |> Enum.uniq()) != length end)
-    |> Enum.at(-1)
+    |> Stream.drop_while(fn {chunk, _} -> length(chunk |> Enum.uniq()) != length end)
+    |> Enum.at(0)
   end
 end
